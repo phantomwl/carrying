74adfce7a50141640b6823a74c53f09b766d6ef4
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-08-28 08:43:32 +0800

    初步搭建框架

---
+++ carrying-builder/src/main/java/com/github/ompc/carrying/DataBuilder.java
@@ -0,0 +1,11 @@
+package com.github.ompc.carrying;
+
+/**
+ * 数据构造者
+ * Created by vlinux on 14-8-28.
+ */
+public class DataBuilder {
+
+
+
+}
---
+++ carrying-server/src/main/java/com/github/ompc/carrying/CarryingServer.java
@@ -0,0 +1,7 @@
+package com.github.ompc.carrying;
+
+/**
+ * Created by vlinux on 14-8-28.
+ */
+public class CarryingServer {
+}
