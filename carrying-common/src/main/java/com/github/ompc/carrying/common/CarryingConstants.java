6b54e15e625891412dd738a8755c37c095c8aea2
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-08-28 10:26:52 +0800

    初步搭建框架

---
+++ carrying-common/src/main/java/com/github/ompc/carrying/common/CarryingConstants.java
@@ -0,0 +1,14 @@
+package com.github.ompc.carrying.common;
+
+/**
+ * 常量定义
+ * Created by vlinux on 14-8-28.
+ */
+public class CarryingConstants {
+
+    /**
+     * 一行最大长度
+     */
+    public static final short MAX_LINE_LEN = 200;
+
+}
