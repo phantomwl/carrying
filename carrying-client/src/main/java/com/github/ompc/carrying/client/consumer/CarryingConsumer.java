2022affe5059d8be92a5b07c44f6f51cbba532c1
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-09-07 18:04:20 +0800

    提升性能

--- carrying-client/src/main/java/com/github/ompc/carrying/client/consumer/CarryingConsumer.java
+++ carrying-client/src/main/java/com/github/ompc/carrying/client/consumer/CarryingConsumer.java
@@ -69,7 +69,7 @@ public class CarryingConsumer {
         socket.connect(option.serverAddress);
         dis = new DataInputStream(socket.getInputStream());
 //        dos = new DataOutputStream(socket.getOutputStream());
-        dos = new DataOutputStream(new CorkBufferedOutputStream(socket.getOutputStream(), option.sendBufferSize, 10));
+        dos = new DataOutputStream(new CorkBufferedOutputStream(socket.getOutputStream(), option.sendBufferSize, 8));
         logger.info("connect to server={} successed.", option.serverAddress);
     }
 
--- carrying-server/src/main/java/com/github/ompc/carrying/server/provider/CarryingProvider.java
+++ carrying-server/src/main/java/com/github/ompc/carrying/server/provider/CarryingProvider.java
@@ -67,7 +67,7 @@ public class CarryingProvider {
                             final DataInputStream dis = new DataInputStream(socket.getInputStream());
                             final DataOutputStream dos =
                                     //new DataOutputStream(socket.getOutputStream());
-                                    new DataOutputStream(new CorkBufferedOutputStream(socket.getOutputStream(), option.childSendBufferSize, 4));
+                                    new DataOutputStream(new CorkBufferedOutputStream(socket.getOutputStream(), option.childSendBufferSize, 8));
 
                             while (socket.isConnected()) {
 
