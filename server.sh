3b62bc074bdb9487f763126531908cd9d58928e9
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-09-07 18:47:41 +0800

    提升性能

--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Xmx8G -Xms8G -Xmn4G -jar ./carrying-server/target/carrying-server-jar-with-dependencies.jar ${1} ${2}
+java -Xmx2G -Xms2G -Xmn1G -jar ./carrying-server/target/carrying-server-jar-with-dependencies.jar ${1} ${2}
