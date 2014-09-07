901360f847a8ae8364a66a4d7cee549cd05d4d40
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-09-07 17:50:28 +0800

    提升性能

--- carrying-common/src/main/java/com/github/ompc/carrying/common/networking/CorkBufferedOutputStream.java
+++ carrying-common/src/main/java/com/github/ompc/carrying/common/networking/CorkBufferedOutputStream.java
@@ -23,7 +23,7 @@ public class CorkBufferedOutputStream extends BufferedOutputStream {
 //
     private volatile boolean isNeedFlush = false;
     private volatile int flushTimes = 0;
-    private static final int MAX_FLUSH_TIMES = 2;
+    private static final int MAX_FLUSH_TIMES = 4;
 
     public CorkBufferedOutputStream(OutputStream out, int size) {
         super(out, size);
