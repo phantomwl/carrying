afa167bb1a2f78cad587bffaa2088536e8898c42
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-09-07 12:20:30 +0800

    提升性能

--- carrying-client/src/main/java/com/github/ompc/carrying/client/CarryingClientLauncher.java
+++ carrying-client/src/main/java/com/github/ompc/carrying/client/CarryingClientLauncher.java
@@ -28,7 +28,7 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 public class CarryingClientLauncher {
 
     private static final int CPU_NUM = Runtime.getRuntime().availableProcessors();
-    private static final int CLI_NUM = CPU_NUM;
+    private static final int CLI_NUM = CPU_NUM * 2;
             //CPU_NUM * 4;
     private static final int CARRIER_NUM = CLI_NUM * 10;
                     // CLI_NUM * 2;
