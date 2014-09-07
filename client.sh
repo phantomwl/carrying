061065559568d9689f065e24184199365f4c0951
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-09-07 08:17:30 +0800

    提升性能

--- client.sh
+++ client.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -jar ./carrying-client/target/carrying-client-jar-with-dependencies.jar ${1} ${2}
+java -Xmx2048m -Xms2048m -jar ./carrying-client/target/carrying-client-jar-with-dependencies.jar ${1} ${2}
